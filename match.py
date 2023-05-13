56598375db97e3c0be71f273accf21ac9e03dad0
OwenHny <76971990+OwenHny@users.noreply.github.com>
2023-05-13 16:11:53 -0500

    bug fixes

--- match.py
+++ match.py
@@ -2,6 +2,8 @@
 import os
 import csv
 
+NAME_LENGTH = 1
+
 class participant:
     def __init__(self, rankings, id, student, slots):
         if(student):
@@ -19,7 +21,7 @@ students = []
 employers = []
 
 # fill student rankings
-student_folder = '/students'
+student_folder = './students'
 
 for filename in os.listdir(student_folder):
     file_path = os.path.join(student_folder, filename)
@@ -40,20 +42,19 @@ for filename in os.listdir(student_folder):
 
             # Extract the data from the first row for each column that has content on the fourth row
             data = [{"id": headers[i], "rank":fourth_row[i]} for i in range(len(headers)) if fourth_row[i]]
-            
-            rankings = [None]* (len(data) - 2)
-            for i in range(len(data)-2):
-                rankings[int(data[i+2]['rank']) - 1] = data[i+2]['id'] 
 
-            students.append(participant(rankings,filename,True,1))
-            #students.append(participant(rankings,data[0]["rank"] + " " + data[1]["rank"],True,1))
+            rankings = [None]* (len(data) - NAME_LENGTH) # TODO: double check subtraction
+            for i in range(len(data)-NAME_LENGTH):
+                rankings[int(data[i + NAME_LENGTH]['rank']) - 1] = data[i + NAME_LENGTH]['id'] 
 
+            #students.append(participant(rankings,filename,True,1))
+            students.append(participant(rankings,data[0]["rank"] ,True,1))
 
 # fill employer rankings
-employer_folder  = '/employers'
+employer_folder  = './employers'
 
-for filename in os.listdir(student_folder):
-    file_path = os.path.join(student_folder, filename)
+for filename in os.listdir(employer_folder):
+    file_path = os.path.join(employer_folder, filename)
     if os.path.isfile(file_path):
         #open file and fill employers 
          with open(file_path, 'r') as file:
@@ -71,11 +72,13 @@ for filename in os.listdir(student_folder):
             # Extract the data from the first row for each column that has content on the fourth row
             data = [{"id": headers[i], "rank":fourth_row[i]} for i in range(len(headers)) if fourth_row[i]]
             
-            rankings = [None]* (len(data) - 2)
-            for i in range(len(data)-2):
-                rankings[int(data[i+2]['rank']) - 1] = data[i+2]['id'] 
+            rankings = [None]* (len(data) - NAME_LENGTH) # TODO: double check subtraction
+            for i in range(len(data) - NAME_LENGTH):
+                rankings[int(data[i + NAME_LENGTH]['rank']) - 1] = data[i + NAME_LENGTH]['id'] 
 
-            employers.append(participant(rankings,filename,False,1))
+            #employers.append(participant(rankings,filename,False,1))
+            employers.append(participant(rankings,data[0]["rank"],False,1))
+
 
 def lowest_rank(rankings, matches):
     low = 0
@@ -88,6 +91,10 @@ def lowest_rank(rankings, matches):
 
     return {'rank':low,'index':index}
 
+def get_employer(id):
+    for employer in employers:
+        if employer.id == id:
+            return employer
 
 cont = True
 # loop until all options are exausted for all students 
@@ -96,11 +103,13 @@ while cont:
     for student in students:
         if ((not student.matched) and student.curr_option < len(student.rankings)): # the student is not currently matched go to next option in list
             cont = True # a student is not matched an could have a match so continue the matching process
-            
-            employer = employers[student.rankings[student.curr_option]] #get next ranked employer
+
+            #print(student.rankings[student.curr_option]) 
+            employer = get_employer(student.rankings[student.curr_option]) #get next ranked employer
 
             # if student is ranked higher than the employers current opption then employer and student are tentativly matched
-            if(employer.rankings.index(student.id) < employer.curr_option): 
+
+            if(student.id in employer.rankings and employer.rankings.index(student.id)  < employer.curr_option): 
                 if(employer.matched == employer.spots):
                     # unset current student matching
 
@@ -138,5 +147,8 @@ while cont:
 for student in students:
     print(student.id, student.matched_ids)
 
+print("\n\n")
+for emp in employers:
+    print(emp.id, emp.matched_ids)
 
 #print stats
