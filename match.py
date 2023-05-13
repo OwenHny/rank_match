558723dcd37d5fa9dc500c9951a6a19a4a7e2dd9
OwenHny <76971990+OwenHny@users.noreply.github.com>
2023-05-12 22:08:03 -0500

    student import started, multiple matches allowed

--- match.py
+++ match.py
@@ -1,14 +1,17 @@
-
+# This file was written with the help of chatGPT
+import os
+import csv
 
 class participant:
-    def __init__(self, rankings, id, student):
+    def __init__(self, rankings, id, student, slots):
         if(student):
             self.curr_option = 0 
         else:
             self.curr_option = len(rankings)
         self.id = id
         self.rankings = rankings
-        self.matched = False
+        self.matched = 0
+        self.spots = slots
         self.matched_id = None
 
 
@@ -16,33 +19,63 @@ students = []
 employers = []
 
 # fill student rankings
+student_folder = '/students'
 
+for filename in os.listdir(student_folder):
+    file_path = os.path.join(student_folder, filename)
+    if os.path.isfile(file_path):
+        #open file and fill students
+        # Open the CSV file
+        with open(file_path, 'r') as file:
+            reader = csv.reader(file)
+
+            # Read the contents of the file
+            csv_contents = list(reader)
+
+            # Get the first row (headers)
+            headers = csv_contents[0]
+
+            # Get the fourth row
+            fourth_row = csv_contents[3]
+
+            # Extract the data from the first row for each column that has content on the fourth row
+            data = [headers[i] for i in range(len(headers)) if fourth_row[i]]
+
+            print(data)
 
 # fill employer rankings
+employer_folder  = '/employers'
+
+for filename in os.listdir(student_folder):
+    file_path = os.path.join(student_folder, filename)
+    if os.path.isfile(file_path):
+        #open file and fill employers 
+        print("")
+
 
 cont = True
 # loop until all options are exausted for all students 
 while cont:  
     cont = False
     for student in students:
-        if (not student.matched and student.curr_option < len(student.rankings)): # the student is not currently matched go to next option in list
+        if (not student.matched < student.spots and student.curr_option < len(student.rankings)): # the student is not currently matched go to next option in list
             cont = True # a student is not matched an could have a match so continue the matching process
             
             employer = employers[student.rankings[student.curr_option]] #get net ranked employer
 
             # if student is ranked higher than the employers current opption then employer and student are tentativly matched
             if(employer.rankings.index(student.id) < employer.curr_option): 
-                if(employer.matched):
+                if(employer.matched == employer.spots):
                     # unset current student matching
                     unmatched_student = students[employer.matched_id]
 
-                    unmatched_student.matched = False
+                    unmatched_student.matched -= 1
                     unmatched_student.curr_option += 1
                     unmatched_student.matched_id = None
 
                 # set match
-                employer.matched = True
-                student.matched = True
+                employer.matched += 1
+                student.matched += 1
 
                 employer.matched_id = student.id
                 student.matched_id = employer.id
